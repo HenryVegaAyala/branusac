4a7f51478be191d3844d971247f6ac2bfa4eea3b
HenryVegaAyala <hp.vega@hotmail.com>
2016-08-05 12:58:23 -0500

    imprenta

--- protected/views/cliente/_form.php
+++ protected/views/cliente/_form.php
@@ -4,79 +4,94 @@
 /* @var $form CActiveForm */
 ?>
 
-<div class="form">
+<div class="container-fluid">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Registrar Nuevo Cliente</h3>
+        </div>
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'cliente-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
-	'enableAjaxValidation'=>false,
-)); ?>
+        <?php $form = $this->beginWidget('CActiveForm', array(
+            'id' => 'cliente-form',
+            'enableAjaxValidation' => false,
+        )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+        <br>
 
-	<?php echo $form->errorSummary($model); ?>
+        <div class="container">
+            <p class="note">Los aspectos con <span class="required letra"> (*) </span> son requeridos.</p>
+        </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'COD_CLIE'); ?>
-		<?php echo $form->textField($model,'COD_CLIE'); ?>
-		<?php echo $form->error($model,'COD_CLIE'); ?>
-	</div>
+        <?php echo $form->errorSummary($model); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'NOMBRE'); ?>
-		<?php echo $form->textField($model,'NOMBRE',array('size'=>60,'maxlength'=>150)); ?>
-		<?php echo $form->error($model,'NOMBRE'); ?>
-	</div>
+        <div class="container-fluid">
+            <div class="fieldset">
+                <div class="form-group">
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'APELLIDO'); ?>
-		<?php echo $form->textField($model,'APELLIDO',array('size'=>45,'maxlength'=>45)); ?>
-		<?php echo $form->error($model,'APELLIDO'); ?>
-	</div>
+                    <div class="col-sm-3">
+                        <?php echo $form->labelEx($model, 'COD_CLIE'); ?>
+                        <?php echo $form->textField($model, 'COD_CLIE', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'COD_CLIE'); ?>
+                    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'RUC'); ?>
-		<?php echo $form->textField($model,'RUC',array('size'=>20,'maxlength'=>20)); ?>
-		<?php echo $form->error($model,'RUC'); ?>
-	</div>
+                    <div class="row">
+                        <?php echo $form->labelEx($model, 'NOMBRE'); ?>
+                        <?php echo $form->textField($model, 'NOMBRE', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'NOMBRE'); ?>
+                    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'DIRECCION'); ?>
-		<?php echo $form->textField($model,'DIRECCION',array('size'=>60,'maxlength'=>150)); ?>
-		<?php echo $form->error($model,'DIRECCION'); ?>
-	</div>
+                    <div class="row">
+                        <?php echo $form->labelEx($model, 'APELLIDO'); ?>
+                        <?php echo $form->textField($model, 'APELLIDO', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'APELLIDO'); ?>
+                    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'TELEFONO'); ?>
-		<?php echo $form->textField($model,'TELEFONO',array('size'=>60,'maxlength'=>60)); ?>
-		<?php echo $form->error($model,'TELEFONO'); ?>
-	</div>
+                    <div class="row">
+                        <?php echo $form->labelEx($model, 'RUC'); ?>
+                        <?php echo $form->textField($model, 'RUC', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'RUC'); ?>
+                    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'FAX'); ?>
-		<?php echo $form->textField($model,'FAX',array('size'=>60,'maxlength'=>60)); ?>
-		<?php echo $form->error($model,'FAX'); ?>
-	</div>
+                    <div class="row">
+                        <?php echo $form->labelEx($model, 'DNI'); ?>
+                        <?php echo $form->textField($model, 'DNI', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'DNI'); ?>
+                    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'CORREO_E'); ?>
-		<?php echo $form->textField($model,'CORREO_E',array('size'=>60,'maxlength'=>150)); ?>
-		<?php echo $form->error($model,'CORREO_E'); ?>
-	</div>
+                    <div class="row">
+                        <?php echo $form->labelEx($model, 'DIRECCION'); ?>
+                        <?php echo $form->textField($model, 'DIRECCION', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'DIRECCION'); ?>
+                    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'ESTADO'); ?>
-		<?php echo $form->textField($model,'ESTADO',array('size'=>1,'maxlength'=>1)); ?>
-		<?php echo $form->error($model,'ESTADO'); ?>
-	</div>
+                    <div class="row">
+                        <?php echo $form->labelEx($model, 'TELEFONO'); ?>
+                        <?php echo $form->textField($model, 'TELEFONO', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'TELEFONO'); ?>
+                    </div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
-	</div>
+                    <div class="row">
+                        <?php echo $form->labelEx($model, 'FAX'); ?>
+                        <?php echo $form->textField($model, 'FAX', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'FAX'); ?>
+                    </div>
 
-<?php $this->endWidget(); ?>
+                    <div class="row">
+                        <?php echo $form->labelEx($model, 'CORREO_E'); ?>
+                        <?php echo $form->textField($model, 'CORREO_E', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'CORREO_E'); ?>
+                    </div>
 
-</div><!-- form -->
+                    <div class="row">
+                        <?php echo $form->labelEx($model, 'ESTADO'); ?>
+                        <?php echo $form->textField($model, 'ESTADO', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'ESTADO'); ?>
+                    </div>
+
+                    <div class="row buttons">
+                        <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+                    </div>
+
+                    <?php $this->endWidget(); ?>
+
+                </div>
+                <!-- form -->
--- protected/views/cliente/create.php
+++ protected/views/cliente/create.php
@@ -2,17 +2,10 @@
 /* @var $this ClienteController */
 /* @var $model Cliente */
 
-$this->breadcrumbs=array(
-	'Clientes'=>array('index'),
-	'Create',
-);
-
-$this->menu=array(
-	array('label'=>'List Cliente', 'url'=>array('index')),
-	array('label'=>'Manage Cliente', 'url'=>array('admin')),
+$this->breadcrumbs = array(
+    'Cliente' => array('index'),
+    'Nuevo',
 );
 ?>
 
-<h1>Create Cliente</h1>
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php $this->renderPartial('_form', array('model' => $model)); ?>
